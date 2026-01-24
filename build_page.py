e35bbb910c853d450024cf33186d24b48c315ab8
RustonSharp <qiao_yanqiang@qq.com>
2026-01-25 00:37:34 +0100

    chore(.gitignore): add macOS, Windows, and Linux system files to .gitignore; refactor build_page.py for code cleanliness

--- build_page.py
+++ build_page.py
@@ -2,11 +2,12 @@ def main():
     # 读取模板文件
     with open("templates/template.html", "r", encoding="utf-8") as template_file:
         html_content = template_file.read()
-    
+
     # 写入到index.html
     with open("index.html", "w", encoding="utf-8") as file:
         file.write(html_content)
     print("HTML file created successfully!")
-    
+
+
 if __name__ == "__main__":
     main()
